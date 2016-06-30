699d755ab50487493cd6d92caacea8f9b974940a
Bastien LEMALE <bastien.lemale@teads.tv>
2016-06-30 18:57:57 +0200

    Add UTs for conditions

---
+++ src/test/scala/com/github/blemale/fuse/FailureCountSpec.scala
@@ -0,0 +1,63 @@
+package com.github.blemale.fuse
+
+import com.github.blemale.fuse.Condition.FailureCount
+import org.scalatest.{ FunSuite, ShouldMatchers, WordSpec }
+
+class FailureCountSpec extends WordSpec with ShouldMatchers {
+
+  "FailureCount" should {
+    "be true" when {
+      "failure count is below threshold" in {
+        val condition = new FailureCount(2)
+
+        condition.update(CallStatus.SUCCESS)
+        condition.update(CallStatus.FAILURE)
+
+        condition.isTrue shouldEqual true
+      }
+    }
+    "be false" when {
+      "failure count is equal to threshold" in {
+        val condition = new FailureCount(2)
+
+        condition.update(CallStatus.SUCCESS)
+        condition.update(CallStatus.FAILURE)
+        condition.update(CallStatus.FAILURE)
+
+        condition.isTrue shouldEqual false
+      }
+      "failure count is above threshold" in {
+        val condition = new FailureCount(2)
+
+        condition.update(CallStatus.SUCCESS)
+        condition.update(CallStatus.FAILURE)
+        condition.update(CallStatus.FAILURE)
+        condition.update(CallStatus.FAILURE)
+
+        condition.isTrue shouldEqual false
+      }
+    }
+
+    "count call status open as failure" in {
+      val condition = new FailureCount(2)
+
+      condition.update(CallStatus.OPEN)
+      condition.update(CallStatus.OPEN)
+
+      condition.isTrue shouldEqual false
+    }
+
+    "reset the condition" in {
+      val condition = new FailureCount(2)
+
+      condition.update(CallStatus.FAILURE)
+      condition.update(CallStatus.FAILURE)
+
+      condition.reset()
+
+      condition.update(CallStatus.FAILURE)
+
+      condition.isTrue shouldEqual true
+    }
+  }
+}
---
+++ src/test/scala/com/github/blemale/fuse/FailureRateSpec.scala
@@ -0,0 +1,79 @@
+package com.github.blemale.fuse
+
+import com.github.blemale.fuse.Condition.{ FailureCount, FailureRate }
+import org.scalatest.{ FunSuite, ShouldMatchers, WordSpec }
+
+class FailureRateSpec extends WordSpec with ShouldMatchers {
+
+  "FailureCount" should {
+    "be true" when {
+      "failure rate is below threshold" in {
+        val condition = new FailureRate(0.5, 3)
+
+        condition.update(CallStatus.SUCCESS)
+        condition.update(CallStatus.SUCCESS)
+        condition.update(CallStatus.FAILURE)
+
+        condition.isTrue shouldEqual true
+      }
+    }
+    "be false" when {
+      "failure rate is equal to threshold" in {
+        val condition = new FailureRate(0.5, 2)
+
+        condition.update(CallStatus.SUCCESS)
+        condition.update(CallStatus.FAILURE)
+
+        condition.isTrue shouldEqual false
+      }
+      "failure count is above threshold" in {
+        val condition = new FailureRate(0.5, 3)
+
+        condition.update(CallStatus.SUCCESS)
+        condition.update(CallStatus.FAILURE)
+        condition.update(CallStatus.FAILURE)
+
+        condition.isTrue shouldEqual false
+      }
+    }
+
+    "count call status open as failure" in {
+      val condition = new FailureRate(0.5, 2)
+
+      condition.update(CallStatus.OPEN)
+      condition.update(CallStatus.OPEN)
+
+      condition.isTrue shouldEqual false
+    }
+
+    "reset the condition" in {
+      val condition = new FailureRate(0.5, 3)
+
+      condition.update(CallStatus.FAILURE)
+      condition.update(CallStatus.FAILURE)
+      condition.update(CallStatus.FAILURE)
+
+      condition.reset()
+
+      condition.update(CallStatus.FAILURE)
+
+      condition.isTrue shouldEqual true
+    }
+
+    "take into account only values in window" in {
+      val condition = new FailureRate(0.5, 3)
+
+      condition.update(CallStatus.FAILURE)
+      condition.update(CallStatus.FAILURE)
+      condition.update(CallStatus.FAILURE)
+
+      condition.isTrue shouldEqual false
+
+      condition.update(CallStatus.SUCCESS)
+      condition.update(CallStatus.SUCCESS)
+
+      condition.isTrue shouldEqual true
+    }
+  }
+
+}
